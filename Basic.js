9fd2cc591d65ca15252c47d791c1d6d520d5470c
KowsalyaT06 <kowsalyat@dorustree.in>
2022-05-02 19:14:08 +0530

    change

--- Basic.js
+++ Basic.js
@@ -1,23 +1,30 @@
-var a = 1;
-a = 5;
-console.log('resss', a);
-//Union 
-var Union;
-Union = 'Types';
-Union = 123;
-var userName;
-userName = 'Kowsi';
-userName = ['Fname', 'Lname'];
-var person;
-person = 'kowsi';
-person = 22;
-var Peopless;
-['Kowsi', 1, 'ramya', 2];
-//Generics
-function g(items) {
-    return items;
-}
-var numbers = g(1);
-var strings = g("kowsi");
-console.log('Numbers', numbers);
-console.log('Strings', strings);
+// let a = 1;
+// a=5
+// console.log('resss',a)
+// //Union 
+// let Union : string|number;
+// Union = 'Types';
+// Union =123;
+// let userName:string|string[]
+// userName='Kowsi'
+// userName=['Fname','Lname']
+// //Alias
+// type People = string|number;
+// let person : People
+// person = 'kowsi'
+// person = 22
+// let Peopless : People[]
+// ['Kowsi',1,'ramya',2]
+// //Generics
+// function g<T>(items:T):T{
+//     return items;
+// }
+// let numbers = g(1);
+// let strings = g("kowsi");
+// console.log('Numbers',numbers);
+// console.log('Strings',strings)
+var employee;
+employee = [
+    { 'firstname': 'testuser', roles: ["data", "data1"], age: 23 },
+    { 'firstname': 'testuser2', roles: ["data2", "data1"], age: 24 }
+];
--- Basic.ts
+++ Basic.ts
@@ -1,37 +1,49 @@
-let a = 1;
-a=5
+// let a = 1;
+// a=5
 
-console.log('resss',a)
+// console.log('resss',a)
 
-//Union 
+// //Union 
 
-let Union : string|number;
-Union = 'Types';
-Union =123;
+// let Union : string|number;
+// Union = 'Types';
+// Union =123;
 
-let userName:string|string[]
-userName='Kowsi'
-userName=['Fname','Lname']
+// let userName:string|string[]
+// userName='Kowsi'
+// userName=['Fname','Lname']
 
-//Alias
+// //Alias
 
-type People = string|number;
+// type People = string|number;
 
 
-let person : People
-person = 'kowsi'
-person = 22
+// let person : People
+// person = 'kowsi'
+// person = 22
 
-let Peopless : People[]
-['Kowsi',1,'ramya',2]
+// let Peopless : People[]
+// ['Kowsi',1,'ramya',2]
 
-//Generics
+// //Generics
 
-function g<T>(items:T):T{
-    return items;
-}
-let numbers = g(1);
-let strings = g("kowsi");
+// function g<T>(items:T):T{
+//     return items;
+// }
+// let numbers = g(1);
+// let strings = g("kowsi");
 
-console.log('Numbers',numbers);
-console.log('Strings',strings)
+// console.log('Numbers',numbers);
+// console.log('Strings',strings)
+
+
+let employe:{
+    firstname:string,
+    roles:string[],
+    age:number,
+}[]
+
+employe =[
+    {'firstname':'testuser',roles:["data", "data1"],age:23},
+    {'firstname':'testuser2',roles:["data2", "data1"],age:24}
+]
